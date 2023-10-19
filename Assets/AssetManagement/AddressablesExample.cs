e321fd6e862005060030c1c15c91699bf7233e82
JorgeChuecaUSJ <147511001+JorgeChuecaUSJ@users.noreply.github.com>
2023-10-19 12:31:07 +0200

    Final addressables examples

--- Assets/AssetManagement/AddressablesExample.cs
+++ Assets/AssetManagement/AddressablesExample.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+
+[System.Serializable]
+public class AssetReferenceAudioClip : AssetReferenceT<AudioClip>
+{
+    public AssetReferenceAudioClip(string guid) : base(guid)
+    {
+    }
+
+
+}
+
 public class AddressablesExample : MonoBehaviour
 {
+    public AssetReference m_AssetReference;
+    public AssetLabelReference m_AssetLabel;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            var handle = Addressables.LoadAssetAsync<GameObject>("Assets/AssetManagement/ManyRocks.prefab");
-            handle.Completed += OnAssetLoad;
+            //m_AssetReference.InstantiateAsync();
+
+
+            var handle = Addressables.LoadAssetsAsync<GameObject>(m_AssetLabel, (go) => { Instantiate(go); });
+            //handle.Completed += OnAssetLoad;
+
+            //var handle = Addressables.LoadAssetAsync<GameObject>("Assets/AssetManagement/ManyRocks.prefab");
         }
     }
 
@@ -26,4 +45,25 @@ public class AddressablesExample : MonoBehaviour
             Debug.LogError("Failed to load: " + handle.Result.name);
         }
     }
+
+
 }
+
+
+
+
+
+
+
+
+//
+//
+//    //public AssetReference m_AssetReference;
+//    //public AssetReferenceGameObject m_AssetReferenceGameObject;
+//    //public AssetReferenceAudioClip m_AssetReferenceAudioClip;
+//    //public AssetLabelReference m_AssetLabelReference;
+//    
+//    //Addressables.LoadAssetsAsync<GameObject>(m_AssetLabelReference, (go) => { Instantiate(go); });
+//    //var handle = m_AssetReferenceAudioClip.LoadAssetAsync<AudioClip>() ;
+//    //var handle = m_AssetReference.LoadAssetAsync<GameObject>() ;
+//    //m_AssetReferenceGameObject.InstantiateAsync();
